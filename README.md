5dec94e4fb1bae3e3744b187282a808b9908846d
Doren Proctor <dorenproctor@gmail.com>
2020-08-05 00:32:36 -0700

    run every hour and update README each time

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # scheduled-increment-file
+
+This project is a simple scheduled CI/CD Proof Of Concept using GitHub Actions.
+
+It runs every hour and increments the number in increment.txt then updates the number in the README as well.
+
+It has been run 0 times
--- increment.sh
+++ increment.sh
@@ -1,8 +1,11 @@
+curr=`cat increment.txt`
+next=$(($curr+1))
 i=$((`cat increment.txt` + 1))
-echo incrementing to $i
-echo $i > increment.txt
+echo incrementing to $next
+echo $next > increment.txt
+sed -i "s/$curr/$next/g" README.md
 git status
 git config --global user.email "dorenproctor@gmail.com"
 git config --global user.name "Doren's CI/CD Runner"
-git commit -am "increment to $i"
+git commit -am "increment to $next"
 git push
